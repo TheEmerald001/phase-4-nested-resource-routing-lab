2156323e0e7e20c12b6b3e0832e30990e711b0a6
steve <austines811@gmail.com>
2022-09-29 01:05:20 +0300

    commit tree 7875874d3cd883ae84713a18d866bb8b6e6896d9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end    
     render json: items, include: :user
   end
 
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])      
+    else
+      item = Item.find(params[:id])
+    end
+    render json: item, include: :user
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(params[:item])
+      return render json: item, include: :user
+    else
+      render json: {error: 'not_found'}
+    end
+  end
 end
